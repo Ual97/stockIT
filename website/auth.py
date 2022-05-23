8eb90177baff56a673adbe2c0e665e30e8f46435
sl4 <3940@holbertonschool.com>
2022-05-23 10:10:18 -0300

    added login flash messages

--- website/auth.py
+++ website/auth.py
@@ -1,17 +1,33 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 # This file is a blueprint, it has urls in it
 # We can have url routes in different files because of this
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        fname = request.form.get('fname')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        if len(email) < 4:
+            flash("Invalid email", category='error')
+        elif len(fname) < 3:
+            flash("Invalid name", category='error')
+        elif password1 != password2:
+            flash("passwords dont match", category='error')
+        elif len(password1) < 7:
+            flash("password too short", category='error')
+        else:
+            flash("account created", category='success')
+            pass
+    return render_template("sign_up.html")
+
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
     return "<p>logout</>"
-
-@auth.route('/sign-up')
-def sign_up():
-    return render_template("sign_up.html")
