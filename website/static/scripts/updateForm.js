1bc14b35135c29083a05151cfeebaede056b13d0
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-06-03 14:06:44 -0300

    updated get in function

--- website/static/scripts/updateForm.js
+++ website/static/scripts/updateForm.js
@@ -1,3 +1,18 @@
+
+
+function httpGet(theUrl) {
+  let xmlHttpReq = new XMLHttpRequest();
+  xmlHttpReq.open("GET", theUrl, false); 
+  xmlHttpReq.send(null);
+  const Dict = JSON.parse(xmlHttpReq.responseText);
+  let List = []
+  const results = Dict['results']
+  for (let i = 0; results[i]; i++) {
+    List.push(results[i]['title'])
+  }
+  return List
+}
+
 window.addEventListener('load', function() {
   const elements = document.getElementsByClassName('updateButton');
   console.log(elements);
@@ -6,7 +21,12 @@ window.addEventListener('load', function() {
     element.addEventListener('click', function () {   
       document.querySelector('.Invisible').classList.replace('Invisible', 'update');
       console.log(element.id)
-      /**document.querySelector('.update').setAttribute('id', element.id)*/
+      
+      const response = httpGet(`/inventory/update/${element.id}`)
+      for (var i = 0; response[i]; i++) {
+        document.querySelector('.update').append("<li>" + response[i]);
+}
+
     });
   };
 });
