6dfd8ddc4305de75184f9125a52b48a2473faf57
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-06-15 18:42:27 -0300

    dynamic popup from scanner read first attemp

--- website/static/scripts/showscanner.js
+++ website/static/scripts/showscanner.js
@@ -1,8 +1,68 @@
 async function consult(code_content) {
   if (!isNaN(code_content)) {
+    
+    const data = await (await fetch(`/inventory/${element.id}`)).json()
+
     console.log(code_content);
-    document.querySelector('#search').setAttribute("value", code_content);
-    document.querySelector('#searchbutton').click();
+    
+    document.querySelector('.InvisibleBarcode').setAttribute('class', 'barcode');    
+
+    tableName = document.querySelector('#nameBarcode');
+    tableName.appendChild(data.name);
+    tableBranch = document.querySelector('#branchBarcode');
+    tableBranch.appendChild(data.branch);
+    tableQuantity = document.querySelector('#quantityBarcode');
+    tableQuantity.appendChild(data.quantity);
+    tableCost = document.querySelector('#costBarcode');
+    tableCost.appendChild(data.cost);
+    tablePrice = document.querySelector('#priceBarcode');
+    tablePrice.appendChild(data.price);
+    tableExpiry = document.querySelector('#expiricy');
+    tableExpiriy.appendChild(data.expiry);
+    tableQty = query.querySelector('#qty_reservedBarcode');
+    tableQty.appendChild(data.qty_reserved);
+    document.querySelector('#qr_barcodeUpdate').setAttribute('src', `/static/images/${id}.png`);
+
+    updateButton = querySelector('#updateButton');
+    updateButton.addEventListener('click', function () {
+
+      document.querySelector('#tableBarcode').setAttribute('class', 'Invisible');
+      document.querySelector('#tableBarcodeUpdate').setAttribute('class', '');
+
+      // filling form with corresponding item data:
+      document.querySelector("#updateFormBarcode").setAttribute("action", `/inventory/${element.id}`);
+      const id = document.querySelector("#idUpdate");
+      id.appendChild(document.createTextNode(data.id));
+      document.querySelector("#nameBarcodeUpdate").setAttribute("value", data.name);
+      const branches = document.querySelector("#branchesBarcodeUpdate");
+      for (let i = 0; data.ownerBranches[i]; i++) {
+        const option = document.createElement("option");
+        option.setAttribute("value", data.ownerBranches[i]);
+        option.appendChild(document.createTextNode(data.ownerBranches[i]));
+        option.setAttribute("value", data.ownerBranches[i]);
+        if (data.ownerBranches[i] === data.branch) {
+          option.setAttribute("selected", "selected");
+        }
+        branches.appendChild(option);
+      }
+      document.querySelector("#quantityBarcodeUpdate").setAttribute("value", data.quantity);
+      document.querySelector("#costBarcodeUpdate").setAttribute("value", data.cost);
+      document.querySelector("#priceBarcodeUpdate").setAttribute("value", data.price);
+      document.querySelector("#expiryBarcodeUpdate").setAttribute("value", data.expiry);
+      document.querySelector("#qty_reservedBarcodeUpdate").setAttribute("value", data.qty_reserved);
+      document.querySelector(`#qr_barcodeBarcodeUpdate${data.qr_barcode}`).setAttribute("selected", 'selected');
+      function cancel() {
+        document.querySelector('#tableBarcodeUpdate').setAttribute('class', 'Invisible')
+        branches.innerHTML = "";
+        id.innerHTML = "";
+      }
+      const cancelButton = document.querySelector("#cancelButtonUpdate")
+      cancelButton.addEventListener("click", cancel);
+
+      
+
+    });
+
   }
   else {
     alert("Code is not registered")
