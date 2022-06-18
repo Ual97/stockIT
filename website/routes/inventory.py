a05f181e01088b15021dcb1f281b97376d9e4431
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-06-18 19:45:44 -0300

    implemented search inventory quantity filtering by branch successfully

--- website/routes/inventory.py
+++ website/routes/inventory.py
@@ -1,7 +1,9 @@
+from codecs import strict_errors
 from website import db
 from website.models.product import Product
 from website.models.movements import Movements
 from website.models.branch import Branch
+from website.models.inventory import Inventory
 from flask_login import login_required
 from flask import Blueprint, render_template, request, flash, redirect, jsonify, abort, url_for
 from flask_login import login_required, current_user
@@ -12,7 +14,7 @@ import requests
 inventory = Blueprint('inventory', __name__)
 
 @login_required
-@inventory.route('/inventory', methods=['GET', 'POST'])
+@inventory.route('/inventory', methods=['GET', 'POST'], strict_slashes=False)
 def inventory_page():
     """inventory page"""
 
@@ -21,30 +23,57 @@ def inventory_page():
         flash('Please confirm your account, check your email (and spam folder)', 'error')
         return redirect(url_for('views.home'))
     
-    # consulting products and movements to get the quantity of each product in inventory
-    products = Product.query.filter(Product.owner == current_user.email)
-    branches = Branch.query.filter(Branch.owner == current_user.email)
-    movements = Movements.query.filter(Movements.owner == current_user.email)
+    # consulting stock from all branches
+    stockQuery = Inventory.query.filter_by(owner=current_user.email).all()
 
-    prodList = []
-    for product in products:
-        prodList.append(product.id)
-    
-    branchesList = []
-    for branch in branches:
-        branchesList.append(branch.id)
-    
-    inventoryDict = {}
 
-    for branch in branchesList:
-        for product in prodList:
-            
+    stock = []
 
-    
+    #filling stock dictionary with prod name, quantity, description and product id
+    for item in stockQuery:
+        stockItem = {}
+
+        product = Product.query.filter_by(owner=current_user.email).filter_by(id=item.prod_id).first()
+
+        stockItem['name'] = product.name
+        stockItem['quantity'] = item.quantity
+        stockItem['description'] = product.description
+        stockItem['id'] = item.prod_id
+
+        stock.append(stockItem)
+
+    print(f'\n\n\nlista final:{stockItem}\n\n')
 
     # if user presses Search
     if request.method == 'POST' and "btn-srch" in request.form:
-        search = request.form.get("search")
+        formDict = request.form.to_dict()
 
-        #checks that products are from user
-        userprod = Product.query.filter(Product.owner == current_user.email)
+        print(f'\n\n\nformDict: {formDict}\n\n')
+
+        selectedBranch = formDict.get('selectBranch')
+
+        # filtering by branch
+        if selectedBranch != 'All Branches (default)':
+            selectedBranch = Branch.query.filter_by(owner=current_user.email).filter_by(name=selectedBranch).first()
+            productMovements = Movements.query.filter_by(owner=current_user.email).filter_by(branch_id=selectedBranch.id).all()
+
+            for item in stock:
+                # calculating the current stock for
+                # each branch distinct from the selected
+                currentStock = 0
+                for mov in Movements.query.filter_by(owner=current_user.email).filter(and_(Movements.prod_id == item['id'],
+                                                     Movements.branch_id == selectedBranch.id)).all():
+                    if mov.in_out is True:
+                        currentStock += mov.quantity
+                    elif mov.in_out is False:
+                        currentStock -= mov.quantity
+                item['quantity'] = currentStock
+
+        
+    # branches from user
+    branches = Branch.query.filter_by(owner=current_user.email).all()
+    branchesList = ["All Branches (default)"]
+    for branch in branches:
+        branchesList.append(branch.name)
+
+    return render_template('inventory.html', stock=stock, user=current_user, branches=branchesList)
