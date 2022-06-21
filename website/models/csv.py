0e3fab0165628b32a84af777f68a5ab6bbd5e015
Mateo Victorica <3951@holbertonschool.com>
2022-06-20 21:27:08 -0300

    done landing page as well as minor tweaks

--- website/models/csv.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import FileField, SubmitField 
-from wtforms.validators import InputRequired 
-
-class UploadFileForm(FlaskForm): 
-    file = FileField("File", validators=[InputRequired()]) 
-    submit = SubmitField("Upload File") 
