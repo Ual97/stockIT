353564d35fbe2a17397fc4279b101dbddd912c9e
Mateo Victorica <3951@holbertonschool.com>
2022-06-21 17:07:11 -0300

    added new phots and updated homepage

--- website/static/scripts/showscanner.js
+++ website/static/scripts/showscanner.js
@@ -1,20 +1,20 @@
-async function consult(code_content) {
+async function consult(code_content, type_barcode) {
   if (!isNaN(code_content)) {
-    const data = await (await fetch(`/inventory/${code_content}`)).json()    
+    const data = await (await fetch(`/inventory/${code_content}`)).json()
     console.log(data);
     console.log(code_content);
-    
-    document.querySelector('.InvisibleBarcode').setAttribute('class', 'barcode');    
-    
-    
+
+    document.querySelector('.InvisibleBarcode').setAttribute('class', 'barcode');
+
+
     tableName = document.querySelector('#nameBarcode');
     tableName.appendChild(document.createTextNode(data.name));
     tableQuantity = document.querySelector('#quantityBarcode');
     tableQuantity.appendChild(document.createTextNode(data.quantity));
-    document.querySelector('#qr_barcodeUpdate').setAttribute('src', `/static/images/.${code_content}.png`);
+    document.querySelector('#qr_barcodeUpdate').setAttribute('src', `/static/images/${type_barcode}.${code_content}.png`);
 
     updateButton = document.querySelector('#updateButtonBarcode');
-    
+
   }
   else {
     alert("Code is not registered")
@@ -26,10 +26,17 @@ let scanner = null;
 async function scannerF() {
   scanner = await Dynamsoft.DBR.BarcodeScanner.createInstance();
   scanner.onFrameRead = results => {
+    let type_barcode;
     if (results[0]) {
       const code_content = results[0].barcodeText;
-      console.log(code_content)
-      consult(code_content);
+      if (results[0].barcodeFormatString === "QR_CODE") {
+        type_barcode = "qr";
+      }
+      else {
+        type_barcode = "barcode";
+      }
+      console.log(results.barcodeFormatString);
+      consult(code_content, type_barcode);
       scanner.hide();
     }
 
