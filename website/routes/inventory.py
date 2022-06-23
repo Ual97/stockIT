019ec662b327d3c490e41d2f6fef745b74160804
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-06-23 19:52:07 -0300

    added chart in inventory

--- website/routes/inventory.py
+++ website/routes/inventory.py
@@ -5,7 +5,7 @@ from website.models.inventory import Inventory
 from flask_login import login_required
 from flask import Blueprint, render_template, request, flash, redirect, jsonify, abort, url_for
 from flask_login import login_required, current_user
-from sqlalchemy import and_
+from sqlalchemy import and_, asc
 
 inventory = Blueprint('inventory', __name__)
 
@@ -26,6 +26,8 @@ def inventory_page():
     # store some  product name if some product is searched
     search = formDict.get('search')
     search = search.lower().strip() if search else None
+
+    border_case = False
 
     # consulting stock from all branches
     stockQuery = Inventory.query.filter_by(owner=current_user.email).all()
@@ -72,7 +74,58 @@ def inventory_page():
                 item['quantity'] = currentStock
                 item['branch'] = selectedBranch.name
                 graph_data[item['name']] = item['quantity']
-                print(item)         
+                print(item)
+        selectedBranch = formDict.get('selectBranch')
+
+        if selectedBranch != 'All Branches (default)' and search:
+            prod_id = Product.query.filter_by(name=search).first().id
+            branch_id = Branch.query.filter_by(name=selectedBranch).first().id
+            print(f'\n\n\nbranch_id {prod_id}\n\n')
+            prodMovements = Movements.query.filter((Movements.branch_id==branch_id) & (Movements.prod_id==prod_id) & (Movements.owner==current_user.email)).order_by(asc(Movements.date)).all()
+            graph_data = [['Date', 'Entries', 'Outs']]
+
+            print(f'\n\n\nprodMovements antes del for: {prodMovements}\n\n')
+            prev = None
+            prodMovementsLen = len(prodMovements)
+            for i in range(prodMovementsLen):
+                print(f'\n\n\n{prodMovements[i].__dict__}\n\n')
+                print(f'\n\nmov date : {str(prodMovements[i].date.date())} prev: {prev}\n')
+                if not prev:
+                    new_data = [0, 0, 0]
+                    new_data[0] = str(prodMovements[i].date.date())
+                    if prodMovements[i].in_out == True:
+                        new_data[1] += prodMovements[i].quantity
+                    elif prodMovements[i].in_out == False:
+                        new_data[2] += prodMovements[i].quantity
+                    prev = str(prodMovements[i].date.date())
+                    if i == prodMovementsLen -1:
+                        graph_data.append(new_data)
+                    print(f'\n\nentre al primer if new data: {new_data} prev: {prev}\n')
+                elif prev and str(prodMovements[i].date.date()) == prev:
+                    new_data[0] = str(prodMovements[i].date.date())
+                    if prodMovements[i].in_out == True:
+                        new_data[1] += prodMovements[i].quantity
+                    elif prodMovements[i].in_out == False:
+                        new_data[2] += prodMovements[i].quantity
+                    prev = str(prodMovements[i].date.date())
+                    print(f'\n\nentre al segundo if new data: {new_data} prev: {prev}\n')
+                    if i == prodMovementsLen -1:
+                        graph_data.append(new_data)
+                elif prev and str(prodMovements[i].date.date()) != prev:
+                    graph_data.append(new_data)
+                    new_data = [0, 0, 0]
+                    new_data[0] = str(prodMovements[i].date.date())
+                    if prodMovements[i].in_out == True:
+                        new_data[1] += prodMovements[i].quantity
+                    elif prodMovements[i].in_out == False:
+                        new_data[2] += prodMovements[i].quantity
+                    prev = str(prodMovements[i].date.date())
+                    if i == prodMovementsLen -1:
+                        graph_data.append(new_data)
+            border_case = True
+
+
+
 
         if selectedBranch == 'All Branches (default)' and search:
             for selectedBranch in Branch.query.filter_by(owner=current_user.email).all():
@@ -98,7 +151,8 @@ def inventory_page():
     for branch in branches:
         branchesList.append(branch.name)
 
-    return render_template('inventory.html', stock=stock, user=current_user, branches=branchesList, data=graph_data)
+    print(f'\n\n\nla data: {graph_data}\n\n')
+    return render_template('inventory.html', stock=stock, user=current_user, branches=branchesList, data=graph_data, border_case=border_case)
 
 
 @login_required
