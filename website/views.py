ff7a1b3409367e7c786a59038c0b6f344bc94bb0
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-06-25 16:03:17 -0300

    implementing limit of request per minute to avoid atacks

--- website/views.py
+++ website/views.py
@@ -1,11 +1,13 @@
 from flask import Blueprint, render_template
 from flask_login import current_user
+from website import limiter
 
 # This file is a blueprint, it has urls in it
 # We can have url routes in different files because of this
 views = Blueprint('views', __name__)
 
 @views.route('/')
+@limiter.limit("10/minute")
 def home():
     if current_user.is_authenticated:
         return render_template("home.html", user=current_user)
