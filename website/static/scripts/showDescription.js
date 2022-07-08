d26b382ecddc3258ceaf485ec6f1b40fad740188
Mateo Victorica <3951@holbertonschool.com>
2022-07-08 17:00:13 -0300

    modified popups

--- website/static/scripts/showDescription.js
+++ website/static/scripts/showDescription.js
@@ -2,30 +2,10 @@ function showDescription(obj) {
 
     const description = obj.getAttribute("value");
     console.log(description);
-    descriptionDiv = document.querySelector("#InvisibleDescription");
     const idTag = obj.getAttribute("id");
-    if (idTag !== "descriptionBarcode")
-    {    
-        descriptionDiv.classList.replace("Invisible", "description");
-    }
-    else
-    {
-        descriptionDiv.setAttribute("class", "");
-    }
     textNode = document.querySelector('#productDescriptionText');
     textNode.innerHTML = description;
     function close() {
-        if (idTag !== "descriptionBarcode")
-    {    
-        document.querySelector("#InvisibleDescription").classList.replace("description", "Invisible");
-    }
-    else
-    {
-        descriptionDiv.setAttribute("class", "InvisibleDescription");
-    }
-    
         textNode.innerHTML = "";
     }
-    const closeButton = document.querySelector("#closeProductDescription");
-    closeButton.addEventListener("click", close);
 }
