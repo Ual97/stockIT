a9a176305eaa1cb5b57c953120b331369969de5a
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-07-11 19:58:52 -0300

    implemented update pop up from products and inventory

--- website/static/scripts/updateFormBranch.js
+++ website/static/scripts/updateFormBranch.js
@@ -4,21 +4,13 @@ window.addEventListener('load', function() {
     for (let element of elements) {
       console.log(element);
       element.addEventListener('click', async function () { //asyc function which allows make await returns
-        document.querySelector('.Invisible').classList.replace('Invisible', 'update');
+        $('#InvisibleBranch').modal('show');
         console.log(element.id)
         // await in fetch return which with wait to conclude the request to then return the json obj
         const data = await (await fetch(`/branch/${element.id}`)).json()
         // filling form with corresponding branch data:
         document.querySelector("#updateForm").setAttribute("action", `/branch/${element.id}`);
-        const id = document.querySelector("#idUpdate");
-        id.appendChild(document.createTextNode(data.id));
         document.querySelector("#nameUpdate").setAttribute("value", data.name);
-        function cancel() {
-          document.querySelector('.update').classList.replace('update', 'Invisible')
-          id.innerHTML = "";
-        }
-        const cancelButton = document.querySelector("#cancelButtonUpdate")
-        cancelButton.addEventListener("click", cancel);
      
       });
     };
--- website/static/scripts/updateFormInventory.js
+++ website/static/scripts/updateFormInventory.js
@@ -2,7 +2,7 @@ window.addEventListener('load', function () {
   const elements = document.getElementsByClassName('updateButton');
   for (let element of elements) {
     element.addEventListener('click', async function () { //asyc function which allows make await returns
-      document.querySelector('.Invisible').classList.replace('Invisible', 'update');
+      $('#InvisibleProduct').modal('show');
       // await in fetch return which with wait to conclude the request to then return the json obj
       console.log(`\n\nel clickeado${element.id}\n`)
       const data = await (await fetch(`/product/${element.id}`)).json()
@@ -10,19 +10,22 @@ window.addEventListener('load', function () {
       console.log(`\n\ndiccionario\n`)
       console.log(data)
       document.querySelector("#updateForm").setAttribute("action", `/product/${element.id}`);
-      const id = document.querySelector("#idUpdate");
-      id.appendChild(document.createTextNode(data.id));
       nameTxt = document.querySelector("#nameUpdate");
+
+      console.log("khe loceee")
+      if (nameTxt.firstChild){
+        console.log(nameTxt.firstChild.length)
+      }
+      console.log("bolas")
+      console.log(data.name)
+      if (nameTxt.firstChild) {
+        nameTxt.removeChild(nameTxt.firstChild)
+      }
       nameTxt.appendChild(document.createTextNode(data.name));
+      console.log(nameTxt)
       console.log(`adasdasd${data.description}`);
       document.querySelector(`#qr_barcodeUpdate${data.qr_barcode}`).setAttribute("selected", 'selected');
       document.querySelector("#descriptionUpdate").setAttribute("value", data.description);
-      function cancel() {
-        document.querySelector('.update').classList.replace('update', 'Invisible')
-        id.innerHTML = "";
-      }
-      const cancelButton = document.querySelector("#cancelButtonUpdate")
-      cancelButton.addEventListener("click", cancel);
     });
   };
 });
